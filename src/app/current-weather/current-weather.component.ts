8b0ac96b17c4d4038dfdaa4a193d9552d519bad5
ArunJHiremath <47742101+ArunJHiremath@users.noreply.github.com>
2019-08-21 00:15:52 +0530

    updated app

---
+++ src/app/current-weather/current-weather.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-current-weather',
+  template: `
+    <p>
+      current-weather works!
+    </p>
+  `,
+  styleUrls: ['./current-weather.component.css']
+})
+export class CurrentWeatherComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
